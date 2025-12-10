7446e604c160e200fe0de6b223eb236b14523939
zhiqiang-hhhh <seuhezhiqiang@163.com>
2025-12-10 16:35:00 +0800

    remove chore

--- sysbench/etl2.py
+++
@@ -1,8 +0,0 @@
-input_file = '/mnt/disk1/hezhiqiang/Code/snippet/sysbench/fe.audit.log.0.cleaned'
-output_file = '/mnt/disk1/hezhiqiang/Code/snippet/sysbench/fe.audit.log.0.cleaned.modified'
-
-with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-    for line in infile:
-        line = line.strip()
-        if line:
-            outfile.write(f'"{line}",\n')
